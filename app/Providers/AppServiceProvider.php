1521622f552414b04acd73fe8acac2d3f700011d
Nur Arifin <its.arivin@gmail.com>
2025-01-25 19:06:12 +0700

    update 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         // Custom blade directive for role check
         Blade::if('role', function ($role) {
             return Auth::user()->role->slug == $role;
